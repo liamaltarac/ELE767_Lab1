d6514db0e2c245bf8fd8184ecffa2ab7a381352a
Liam Altarac <liamaltarac@gmail.com>
2019-02-04 15:51:37 -0500

    deleted rar files

--- couche.py
+++ couche.py
@@ -6,12 +6,13 @@ class couche(object):
         self.neurones = []
         self.entrees = []
         self.sorties = []
+        
         self.poids = Poids()
         for i in range(numOfNeurones):
             for i in range(len):
                 self.poids
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     c = couche()
-    c.
+    
